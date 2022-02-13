cadd02b315a97841681ef7c53f4e9ee4be80fd6e
Andreev Danila <mail@neandreev.ru>
2022-02-13 21:15:48 +0500

    remove old code

--- src/modules/training/atoms/AnswerInform/AnswerInform.tsx
+++
@@ -1,9 +0,0 @@
-import { useAppSelector } from '../../../../hooks';
-import { selectCurrentQuestion } from '../../../../features/training/trainingSlice';
-
-export const AnswerInform: React.FC = () => {
-	const question = useAppSelector(selectCurrentQuestion);
-	const informText = question.wasAnsweredCorrectly ? 'Верно!' : 'Неверно!';
-
-	return <span>{informText}</span>;
-};

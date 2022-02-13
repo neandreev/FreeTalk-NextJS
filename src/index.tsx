9dba5eac79540634bc46be929ca8fcbf8c5faa7f
Andreev Danila <mail@neandreev.ru>
2022-02-13 21:31:02 +0500

    fix store

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 import { App } from './App';
-import { store } from './store';
+import { store } from './store/store';
 
 import { ProvideAuth } from './hooks/useAuth';
 

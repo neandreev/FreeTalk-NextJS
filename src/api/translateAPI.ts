85856064477ba7679521be3cbaff5497f820af9a
Aleksandr <65610825+A-Apkhanov@users.noreply.github.com>
2022-02-15 23:19:58 +0300

    Add env (#14)

--- src/api/translateAPI.ts
+++ src/api/translateAPI.ts
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 
-const API_KEY =
-	'dict.1.1.20200629T110424Z.820c51d0c3c6ce08.edfcf3c77862c014dce8158ae4581d8a842e9b30';
+const API_KEY = process.env.REACT_APP_TRANSLATE_API_KEY as string;
 
 interface IResponse {
 	ru: string;
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,13 +3,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 firebase.initializeApp({
-	apiKey: 'AIzaSyAudkAgWP66xM7S38SC2D-Ls75YzLmeZ3s',
-	authDomain: 'freetalk2022.firebaseapp.com',
-	databaseURL: 'https://freetalk2022-default-rtdb.firebaseio.com',
-	projectId: 'freetalk2022',
-	storageBucket: 'freetalk2022.appspot.com',
-	messagingSenderId: '1003236681631',
-	appId: '1:1003236681631:web:1a2ec2c0e896b494a87faa',
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export type User = firebase.User | null | false;

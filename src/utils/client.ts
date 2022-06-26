6e2f6528397ca7ae46699f5639db2c11b6e83a49
Andreev Danila <mail@neandreev.ru>
2022-06-26 18:47:02 +0500

    remove unused trpc client

--- src/utils/client.ts
+++
@@ -1,6 +0,0 @@
-import type { AppRouter } from '../../pages/api/trpc/[trpc]';
-import { createTRPCClient } from '@trpc/client';
-
-export const client = createTRPCClient<AppRouter>({
-  url: 'http://localhost:5000/trpc',
-});

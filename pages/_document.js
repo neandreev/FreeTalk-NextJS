731eb905de48d79442727858878e8db794d8c6a6
Andreev Danila <mail@neandreev.ru>
2022-06-28 23:59:17 +0500

    add title and meta description

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang='ru'>
-			<Head title='FreeTalk' />
+			<Head>
+				<title>FreeTalk</title>
+				<meta name='description' content='Сайт-сервис для изучения иностранных слов' />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />

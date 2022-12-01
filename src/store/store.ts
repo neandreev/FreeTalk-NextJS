2f9d51152c1e722cacdc3c13659968391e3c7ea9
Danila Andreev <mail@neandreev.ru>
2022-12-01 22:57:19 +0500

    Remove unused collection store slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { collectionReducer } from '@/features/collections/collectionsSlice';
 import { dictionaryReducer } from '@/features/dictionary/dictionarySlice';
 import { translateReducer } from '@/features/translate/translateSlice';
 import { trainingReducer } from '@/features/training/trainingSlice';
@@ -8,7 +7,6 @@ import { trainingReducer } from '@/features/training/trainingSlice';
 export const store = configureStore({
 	reducer: {
 		translate: translateReducer,
-		collections: collectionReducer,
 		dictionary: dictionaryReducer,
 		training: trainingReducer,
 	},

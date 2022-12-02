a8f85a1cecbe04b2f45ca329b0b5fdc18e219219
Danila Andreev <mail@neandreev.ru>
2022-12-02 19:52:22 +0500

    Refactor Training component

--- src/components/organism/Training/Training.tsx
+++ src/components/organism/Training/Training.tsx
@@ -1,126 +1,150 @@
 import _shuffle from 'lodash-es/shuffle';
-import { FC, MouseEventHandler, useEffect, useState } from 'react';
+import {
+  FC,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
-import Quiz from '../Quiz';
-import { RepeatTraining } from '../../atoms/RepeatTraining';
-
-import { IQuestion } from '../../../interfaces/question';
-import { TrainingIntro } from '../../atoms/TrainingIntro';
-
-import style from './Training.module.css';
 import { Col, Row } from 'antd';
-import trpc from '../../../utils/trpc';
+
 import { useSession } from 'next-auth/react';
 import { LearningWord } from '@prisma/client';
 import { useStore } from '@/store/store';
 import shallow from 'zustand/shallow';
+import Quiz from '../Quiz';
+
+import { RepeatTraining } from '../../atoms/RepeatTraining';
+import { IQuestion } from '../../../interfaces/question';
+import { TrainingIntro } from '../../atoms/TrainingIntro';
+
+import style from './Training.module.css';
+import trpc from '../../../utils/trpc';
 
 const generateQuestions = (words: LearningWord[]) => {
-	const questions = words.map((word) => {
-		const correctAnswerId = word.id;
-		const wrongAnswersIds = _shuffle(words)
-			.map((word) => word.id)
-			.filter((wordId) => wordId !== correctAnswerId)
-			.slice(0, 3);
+  const questions = words.map((word) => {
+    const correctAnswerId = word.id;
+    const wrongAnswersIds = _shuffle(words)
+      .map((word) => word.id)
+      .filter((wordId) => wordId !== correctAnswerId)
+      .slice(0, 3);
 
-		const question: IQuestion = {
-			wasAnswered: false,
-			wasAnsweredCorrectly: null,
-			answerId: 0,
-			correctAnswerId,
-			wrongAnswersIds,
-		};
+    const question: IQuestion = {
+      wasAnswered: false,
+      wasAnsweredCorrectly: null,
+      answerId: 0,
+      correctAnswerId,
+      wrongAnswersIds,
+    };
 
-		return question;
-	});
+    return question;
+  });
 
-	return questions;
+  return questions;
 };
 
 const selectWordsForTraining = (words: LearningWord[]) => {
-	const availableWordsForTraining = words.filter((word) => {
-		const timestamp = Date.now();
-		return word.timeToTrain * 1000 <= timestamp && !word.learned;
-	});
-	const shuffledWords = _shuffle(availableWordsForTraining);
-	const wordsForTraining = shuffledWords.slice(0, 10);
+  const availableWordsForTraining = words.filter((word) => {
+    const timestamp = Date.now();
+    return word.timeToTrain * 1000 <= timestamp && !word.learned;
+  });
+  const shuffledWords = _shuffle(availableWordsForTraining);
+  const wordsForTraining = shuffledWords.slice(0, 10);
 
-	return wordsForTraining;
+  return wordsForTraining;
 };
 
-export const Training: FC = (props) => {
-	const { data: session } = useSession();
-	const email = session?.user?.email || null;
+const Training: FC = () => {
+  const { data: session } = useSession();
+  const email = session?.user?.email || null;
 
-	const [isCompleted, resetTraining, setTrainingWords, setQuestions] = useStore(
-		(state) => [
-			state.isCompleted,
-			state.resetTraining,
-			state.setTrainingWords,
-			state.setQuestions,
-		],
-		shallow
-	);
+  const [isCompleted, resetTraining, setTrainingWords, setQuestions] = useStore(
+    (state) => [
+      state.isCompleted,
+      state.resetTraining,
+      state.setTrainingWords,
+      state.setQuestions,
+    ],
+    shallow
+  );
 
-	const wordsQuery = trpc.useQuery(['words', email]);
-	const isLoading = wordsQuery.isLoading;
-	const words = wordsQuery.data || [];
+  const wordsQuery = trpc.useQuery(['words', email]);
+  const { isLoading } = wordsQuery;
+  const words = useMemo(() => wordsQuery.data || [], [wordsQuery.data]);
+  const wordsForTraining = useMemo(
+    () => selectWordsForTraining(words),
+    [words]
+  );
+  const questions = useMemo(
+    () => generateQuestions(wordsForTraining),
+    [wordsForTraining]
+  );
 
-	const [startTraining, setStartTraining] = useState(false);
-	const [isDataPrepared, setIsDataPrepared] = useState(false);
-	const [isTrainingAvailable, setIsTrainingAvailable] = useState(false);
-	const [isTrainingRepeated, setIsTrainingRepeated] = useState(false);
+  const [startTraining, setStartTraining] = useState(false);
+  const [isDataPrepared, setIsDataPrepared] = useState(false);
+  const [isTrainingAvailable, setIsTrainingAvailable] = useState(false);
+  const [isTrainingRepeated, setIsTrainingRepeated] = useState(false);
 
-	const handleResetTraining = () => {
-		resetTraining();
-		setStartTraining(false);
-		setIsDataPrepared(false);
-		setIsTrainingAvailable(false);
-		setIsTrainingRepeated(true);
-	};
+  const handleResetTraining = useCallback(() => {
+    resetTraining();
+    setStartTraining(false);
+    setIsDataPrepared(false);
+    setIsTrainingAvailable(false);
+    setIsTrainingRepeated(true);
+  }, [resetTraining]);
 
-	useEffect(() => {
-		if (!isLoading || isTrainingRepeated) {
-			const wordsForTraining = selectWordsForTraining(words!);
-			const questions = generateQuestions(wordsForTraining);
+  const handleStartTraining: MouseEventHandler = () => {
+    setStartTraining(true);
+  };
 
-			setTrainingWords(wordsForTraining);
-			setQuestions(questions);
+  useEffect(() => {
+    if ((!isLoading || isTrainingRepeated) && !startTraining) {
+      setTrainingWords(wordsForTraining);
+      setQuestions(questions);
 
-			if (wordsForTraining.length >= 10) {
-				setIsTrainingAvailable(true);
-			}
+      if (wordsForTraining.length >= 10) {
+        setIsTrainingAvailable(true);
+      }
 
-			setIsDataPrepared(true);
-			setIsTrainingRepeated(false);
-		}
+      setIsDataPrepared(true);
+      setIsTrainingRepeated(false);
+    }
+  }, [
+    isLoading,
+    isTrainingRepeated,
+    setQuestions,
+    setTrainingWords,
+    handleResetTraining,
+    wordsForTraining,
+    questions,
+    startTraining,
+  ]);
 
-		return handleResetTraining;
-	}, [isLoading, isTrainingRepeated]);
+  useEffect(() => handleResetTraining, [handleResetTraining]);
 
-	const handleStartTraining: MouseEventHandler = (e) => {
-		setStartTraining(true);
-	};
-
-	return (
-		<Row justify='center' align='middle'>
-			<Col className='training'>
-				<h1 className={`page__title ${style.title}`}>Тренировка</h1>
-				<hr />
-				{!isLoading && isDataPrepared && startTraining ? (
-					<Quiz />
-				) : (
-					<TrainingIntro
-						isDataPrepared={isDataPrepared}
-						isTrainingAvailable={isTrainingAvailable}
-						handleStart={handleStartTraining}
-					/>
-				)}
-				<RepeatTraining
-					isCompleted={isCompleted}
-					handleReset={handleResetTraining}
-				/>
-			</Col>
-		</Row>
-	);
+  return (
+    <Row justify="center" align="middle">
+      <Col className="training">
+        <h1 className={`page__title ${style.title}`}>Тренировка</h1>
+        <hr />
+        {!isLoading && isDataPrepared && startTraining ? (
+          <Quiz />
+        ) : (
+          <TrainingIntro
+            isDataPrepared={isDataPrepared}
+            isTrainingAvailable={isTrainingAvailable}
+            handleStart={handleStartTraining}
+          />
+        )}
+        <RepeatTraining
+          isCompleted={isCompleted}
+          handleReset={handleResetTraining}
+        />
+      </Col>
+    </Row>
+  );
 };
+
+export default Training;

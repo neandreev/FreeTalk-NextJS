236cdfa387c24b5c830c2dcdc902553fb181450f
Danila Andreev <mail@neandreev.ru>
2022-12-07 04:47:54 +0500

    Refactor _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,76 +1,68 @@
-import { ConfigProvider } from 'antd';
-import { SessionProvider } from 'next-auth/react';
-import ruRU from 'antd/lib/locale/ru_RU';
-
+import Head from 'next/head';
+import superjson from 'superjson';
+import { withTRPC } from '@trpc/next';
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
 import { loggerLink } from '@trpc/client/links/loggerLink';
+import { SessionProvider } from 'next-auth/react';
+import { ConfigProvider, Layout } from 'antd';
 
-import type { AppProps } from 'next/app'
-import superjson from 'superjson';
+import ruRU from 'antd/lib/locale/ru_RU';
 
-import { AppRouter } from './api/trpc/[trpc]';
-import { withTRPC } from '@trpc/next';
+import { AppProps } from 'next/app';
 
-import { AppType } from 'next/dist/shared/lib/utils';
+import { AppRouter } from 'pages/api/trpc/[trpc]';
 
-import Header from '../src/components/organism/Header';
-import Footer from '../src/components/organism/Footer';
-import { Layout } from 'antd';
+import Header from '@/components/organism/Header';
+import Footer from '@/components/organism/Footer';
 
 import '../src/index.css';
-import Head from "next/head";
 
 const { Content } = Layout;
 
 const FreeTalk = ({
-	Component,
-	pageProps: { session, ...pageProps },
-}: AppProps) => {
-	return (
-		<SessionProvider session={session}>
-			<ConfigProvider locale={ruRU}>
-				<Head>
-					<title>FreeTalk</title>
-				</Head>
-				<Layout className='layout'>
-					<Header />
-						<Content className='content container'>
-							<Component {...pageProps} />
-						</Content>
-					<Footer />
-				</Layout>
-			</ConfigProvider>
-		</SessionProvider>
-	);
-};
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => (
+  <SessionProvider session={session}>
+    <ConfigProvider locale={ruRU}>
+      <Head>
+        <title>FreeTalk</title>
+      </Head>
+      <Layout className="layout">
+        <Header />
+        <Content className="content container">
+          <Component {...pageProps} />
+        </Content>
+        <Footer />
+      </Layout>
+    </ConfigProvider>
+  </SessionProvider>
+);
 
 function getBaseUrl() {
   if (typeof window !== 'undefined') {
     return '';
   }
-  // reference for vercel.com
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
 export default withTRPC<AppRouter>({
-	config({ ctx }) {
-		if (typeof window !== 'undefined') {
-			return {
-				transformer: superjson,
-				url: '/api/trpc',
-			};
-		}
+  config() {
+    if (typeof window !== 'undefined') {
+      return {
+        transformer: superjson,
+        url: '/api/trpc',
+      };
+    }
 
-		const url = `${getBaseUrl()}/api/trpc`;
+    const url = `${getBaseUrl()}/api/trpc`;
 
-		return {
-			links: [
-        // adds pretty logs to your console in development and logs errors in production
+    return {
+      links: [
         loggerLink({
           enabled: (opts) =>
             process.env.NODE_ENV === 'development' ||
@@ -80,28 +72,26 @@ export default withTRPC<AppRouter>({
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
-			transformer: superjson,
-			url,
-			headers: {
-				'x-ssr': '1',
-			},
-		};
-	},
-	ssr: true,
-	responseMeta({ ctx, clientErrors }) {
-		if (clientErrors.length) {
-			// propagate http first error from API calls
-			return {
-				status: clientErrors[0].data?.httpStatus ?? 500,
-			};
-		}
+      transformer: superjson,
+      url,
+      headers: {
+        'x-ssr': '1',
+      },
+    };
+  },
+  ssr: true,
+  responseMeta({ clientErrors }) {
+    if (clientErrors.length) {
+      return {
+        status: clientErrors[0].data?.httpStatus ?? 500,
+      };
+    }
 
-		// cache request for 1 day + revalidate once every second
-		const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-		return {
-			headers: {
-				'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-			},
-		};
-	},
+    const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+    return {
+      headers: {
+        'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+      },
+    };
+  },
 })(FreeTalk);
